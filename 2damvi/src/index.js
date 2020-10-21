710f203a33495aa64490791cb90a341fae53882e
davido1323 <60893031+davido1323@users.noreply.github.com>
2020-10-21 17:13:51 +0200

    Update

--- 2damvi/src/index.js
+++ 2damvi/src/index.js
@@ -6,16 +6,58 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 let gamer = {
+    //posicio : '',
+    //alias : '',
     nombre: '',
     apellido: '',
     score: ''
 };
+
+/**let jugadores = {
+    posicio: '',
+    alies: '',
+    nom: '',
+    congnom: '',
+    score: ''
+};**/
+
+let jugadores = [{
+    posicio: 3,
+    alies: "jperez",
+    nom: "Jose",
+    congnom: "Perez",
+    score: 1000
+},
+{
+    posicio: 14,
+    alies: "jsanz",
+    nom: "Juan",
+    congnom: "Sanz",
+    score: 300
+},
+{
+    posicio: 2,
+    alies: "mgutierrez",
+    nom: "Maria",
+    congnom: "Gutierrez",
+    score: 850
+}
+]
 
 let respuesta = {
     error: false,
     codigo: 200,
     mensaje: ''
 };
+
+app.get('/', function (req, res){
+    respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: 'hola'
+    }
+    res.send(respuesta);
+});
 
 app.post('/gamer', function (req, res) {
     var nom = req.body.nombre || null;
@@ -62,6 +104,15 @@ app.get ("/hola", function (req, res){
     res.send('[GET]Saludos');
 });
 
-app.listen(3000, () => {
-    console.log('El servidor esta inicializado en el puerto 3000');
+app.get ("/ranking", function (req, res){
+    jugadores.sort((a,b) => (a.score < b.score ? 1: -1));
+    for (i = 0; i < jugadores.length; i++)
+    {
+        jugadores[i].posicio = i + 1;
+    }
+    res.send(jugadores);
+});
+
+app.listen(2999, () => {
+    console.log('El servidor esta inicializado en el puerto 2999');
 });
